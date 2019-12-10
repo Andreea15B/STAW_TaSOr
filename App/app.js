ce63656d45c4ff8b32eccde350bd2fab1079d47d
MayaMaria <mariabulboaca@yahoo.com>
2019-12-10 15:16:24 +0200

    try to create server

--- App/app.js
+++ App/app.js
@@ -3,11 +3,14 @@ const app = express();
 const PORT = 3000;
 const path = require('path');
 
+var loginRouter = require('./routes/login');
 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/login.html'));
+// })
+
+app.use('/', loginRouter);
 
 app.use(express.static(__dirname + '/public'));
 
