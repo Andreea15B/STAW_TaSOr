e755c133b42b83ae5ff2a605eb8aa20d6032e482
MayaMaria <mariabulboaca@yahoo.com>
2019-12-30 17:14:01 +0200

    Incomplete login, but works :(

--- App/routes/login.js
+++ App/routes/login.js
@@ -10,16 +10,27 @@ router.post('/login', (req, res) => {
     var { username, password } = req.body;
     var errors = [];
 
-    fetch('http://localhost:3000/users/' + username)
-        .then(response => response.json())
-        .then(response => {
-            if (response.length == 0) {
-                errors.push({ msg: 'User doesn`t exist' });
-            }
-            if (errors.length > 0) res.render('login', { errors });
-            else
-                res.redirect('/register');
-        });
+    if (username == '' || password == '') {
+        errors.push({ msg: 'Fill all the fields' });
+        res.render('login', { errors });
+    } else {
+        fetch('http://localhost:3000/users/' + username)
+            .then(response => response.json())
+            .then(response => {
+                if (response.length == 0) {
+                    errors.push({ msg: 'User doesn`t exist' });
+                } else {
+                    let actual_password = response[0]['password'];
+                    if (actual_password != password) {
+                        errors.push({ msg: 'Incorect password' });
+                    }
+                }
+
+                if (errors.length > 0) res.render('login', { errors });
+                else
+                    res.redirect('/home');
+            });
+    }
 });
 
 router.get('/register', (req, res) => res.send('register'));
