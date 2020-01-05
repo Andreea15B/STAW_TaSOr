3a8657de8f67e93248a61746f057ad7b7769a973
maalex98 <matei.alexandru1498@yahoo.ro>
2020-01-05 16:59:07 +0200

    Create-Board Modal
    Create-Board Modal

--- App/public/javascripts/openmodal.js
+++ App/public/javascripts/openmodal.js
@@ -1,6 +1,6 @@
 var modal = document.getElementById("myModal");
 var button = document.getElementById("myButton");
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("closed")[0];
 
 button.onclick = function() {
     modal.style.display = "block";
