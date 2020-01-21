bb17008e0e3aa2a447dada74a3587ee450239152
MayaMaria <mariabulboaca@yahoo.com>
2020-01-21 13:17:09 +0200

    Resolve problem with empty state for boards

--- App/public/javascripts/errors.js
+++ App/public/javascripts/errors.js
@@ -1,9 +1,8 @@
 var error_container = document.getElementsByClassName('error-container')[0];
-
-console.log('iieeiieiii');
-
 var button = document.getElementsByClassName('click-area')[0];
 
-button.onclick = function() {
-    error_container.style.display = "none";
+if (error_container != undefined) {
+    button.onclick = function() {
+        error_container.style.display = "none";
+    }
 }
