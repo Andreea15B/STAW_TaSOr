303ab397e5be5d9747eda8f24375f208ce32d592
Andreea15B <andreea.bucataru15@yahoo.ro>
2020-01-22 20:34:47 +0200

    get tasks by status

--- App/routes/board.js
+++ App/routes/board.js
@@ -4,13 +4,24 @@ var { ensureAuthenticated } = require("../middleware/auth");
 var fetch = require("node-fetch");
 
 router.get("/", ensureAuthenticated, (req, res) => {
-    const initiale = req.session.username;
-    var status = 'todo';
-    fetch('http://localhost:3000/tasks/' + status)
-        .then(response => response.json())
-        .then(response => {
-            res.render('board', { username: initiale[0].toUpperCase() + initiale[1].toUpperCase(), tasks: response });
-        });
+  const initiale = req.session.username;
+
+  Promise.all([
+    fetch("http://localhost:3000/tasks/todo").then(response => response.json()),
+    fetch("http://localhost:3000/tasks/inprogress").then(response => response.json()),
+    fetch("http://localhost:3000/tasks/done").then(response => response.json())
+  ])
+    .then(response => {
+      res.render("board", {
+        username: initiale[0].toUpperCase() + initiale[1].toUpperCase(),
+        tasksToDo: response[0],
+        tasksInProgress: response[1],
+        tasksDone: response[2]
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
