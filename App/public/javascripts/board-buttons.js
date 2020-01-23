ba57bc4cd7efc4b360d4b7e4d76952889d43ce0e
Andreea15B <andreea.bucataru15@yahoo.ro>
2020-01-23 13:52:49 +0200

    Tasks

--- App/public/javascripts/board-buttons.js
+++ App/public/javascripts/board-buttons.js
@@ -3,9 +3,3 @@ var homeButton = document.getElementById("home-btn");
 homeButton.onclick = function() {
   location.replace("/home");
 };
-
-
-// check if there are tasks in cards
-var inProgressCard = document.getElementById("in-progress-card");
-var doneCard = document.getElementById("done-card");
-
