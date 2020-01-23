1067c44a23136fb927d9b365702ac7c54e695049
Andreea15B <andreea.bucataru15@yahoo.ro>
2020-01-23 16:09:12 +0200

    addTask validation

--- App/routes/task.js
+++ App/routes/task.js
@@ -2,31 +2,44 @@ var express = require("express");
 var router = express.Router();
 var fetch = require("node-fetch");
 
+function formatDate(date) {
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear(),
+    hour = d.getHours(),
+    minutes = d.getMinutes();
+
+  if (month.length < 2)
+    month = '0' + month;
+  if (day.length < 2)
+    day = '0' + day;
+
+  var date_ = [year, month, day].join('-');
+  var hours = [hour, minutes].join('-');
+  return [date_, hours].join(' ');
+}
+
 router.get('/task', (req, res) => res.render('tasks'));
 
 router.post('/', (req, res) => {
   let { title, status, deadline, description, domain, geographical_area } = req.body;
-  let errors = [];
-  if (title == '') {
-    errors.push({ msg: "Fill at least the title" });
-    res.render('board', { errors });
-  } else {
-    if (errors.length > 0) res.render('board', { errors });
-    else {
-      try {
-        const data = { title, status, deadline, description, domain, geographical_area };
-        fetch("http://localhost:3000/tasks/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(data)
-        });
-        res.redirect('/board');
-      } catch {
-        console.log("erori: ", errors);
-      }
-    }
+  if (!deadline) deadline = '0000-00-00';
+  var date_ = formatDate(Date.now());
+  const initiale = req.session.username;
+
+  try {
+    const data = { title, status, created_at: date_, deadline, description, domain, geographical_area };
+    fetch("http://localhost:3000/tasks/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    res.redirect('/board');
+  } catch {
+    console.log("errors");
   }
 });
 module.exports = router;
