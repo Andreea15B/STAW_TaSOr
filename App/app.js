7e316c54338b9b86d85482cf1741598b7831911b
MayaMaria <mariabulboaca@yahoo.com>
2020-02-01 12:13:47 +0200

    Added manifest.json

--- App/app.js
+++ App/app.js
@@ -27,7 +27,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 app.use('/', require('./routes/login'));
 app.use('/register', require('./routes/register'));
 app.use('/home', require('./routes/home'));
